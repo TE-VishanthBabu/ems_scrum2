92c72bc17eba94da3399b8c0853d14eae5889f89
TE-VishanthBabu <91727447+TE-VishanthBabu@users.noreply.github.com>
2022-01-06 12:13:10 +0530

    Delete ems directory

--- ems/ems/src/main/java/com/technoelevatescrum2/ems/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.technoelevatescrum2.ems.service;
-
-import com.technoelevatescrum2.ems.beans.EmployeeInfo;
-
-public interface EmployeeService {
-
-	public EmployeeInfo register(EmployeeInfo info);
-}
